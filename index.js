1573516cf877549ab2f55695552f47ce254bc0b8
Frandorr1 <142456182+Frandorr1@users.noreply.github.com>
2023-08-30 19:48:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // this is a js file
 // Franciele Dorr Dos Santos
+
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
